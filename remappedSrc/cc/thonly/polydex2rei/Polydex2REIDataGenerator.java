44698cb34689b139443f654fc3ef559b39793fdc
UncleTyrone <jeffreycoombs34985@yahoo.com>
2025-11-24 16:00:30 -0500

    Update to 1.21.10

---
+++ remappedSrc/cc/thonly/polydex2rei/Polydex2REIDataGenerator.java
@@ -0,0 +1,11 @@
+package cc.thonly.polydex2rei;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Polydex2REIDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
--- remappedSrc/cc/thonly/polydex2rei/mixin/ScreenOverlayImplMixin.java
+++ remappedSrc/cc/thonly/polydex2rei/mixin/ScreenOverlayImplMixin.java
@@ -1,11 +1,13 @@
 package cc.thonly.polydex2rei.mixin;
 
+
 import cc.thonly.polydex2rei.network.Action;
 import cc.thonly.polydex2rei.network.StackActionPayload;
 import cc.thonly.polydex2rei.util.ClientPolymerItemUtils;
+import lombok.Generated;
 import lombok.extern.slf4j.Slf4j;
+import me.shedaniel.math.impl.PointHelper;
 import me.shedaniel.rei.api.client.config.ConfigObject;
-import me.shedaniel.rei.api.client.gui.widgets.Widget;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
 import me.shedaniel.rei.api.common.entry.EntryStack;
 import me.shedaniel.rei.impl.client.gui.ScreenOverlayImpl;
@@ -19,14 +21,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Slf4j
-@Mixin(value = { ScreenOverlayImpl.class })
+@Mixin(value = {ScreenOverlayImpl.class})
 public class ScreenOverlayImplMixin {
 
-    @Inject(method = { "keyPressed" }, at = { @At(value = "HEAD") }, cancellable = true)
+    @Inject(method = {"keyPressed"}, at = {@At(value = "HEAD")}, cancellable = true)
     public void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        EntryStack<?> stack = ScreenRegistry.getInstance().getFocusedStack(MinecraftClient.getInstance().currentScreen,
-                Widget.mouse());
-
+        EntryStack<?> stack = ScreenRegistry.getInstance().getFocusedStack(MinecraftClient.getInstance().currentScreen, PointHelper.ofMouse());
         if (stack == null) {
             return;
         }
